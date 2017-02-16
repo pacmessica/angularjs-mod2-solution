e353409d0f8b055e941dea42ed29882cde3f8a8a
Jessica Paczuski <jessica@pimmr.com>
2017-02-16 11:31:00 +0100

    Create toBuy and Bought controllers and ShoppingListCheckOff service

--- app.js
+++ app.js
@@ -2,4 +2,23 @@
 'use strict';
 
 angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var toBuy = this;
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+}
+
+
 }()
