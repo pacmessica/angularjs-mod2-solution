edfba38ba68ef620b797de038afebb7d73f1fe5a
Jessica Paczuski <jessica@pimmr.com>
2017-02-16 11:49:56 +0100

    Create item list in service and getitem methods for tobuy and bought controllers

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ angular.module('ShoppingListCheckOff', [])
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
   var toBuy = this;
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
-  var toBuy = this;
+  var bought = this;
+
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
 }
 
 function ShoppingListCheckOffService() {
   var service = this;
+  var items = [
+    {name: "cookies", bought: false},
+    {name: "milk", bought: false},
+    {name: "eggs", bought: false},
+    {name: "pasta", bought: false}
+  ];
+
+  service.getBoughtItems = function() {
+    return items.filter(item => item.bought)
+  }
+
+  service.getToBuyItems = function() {
+    return items.filter(item => !item.bought)
+  }
 }
 
 
-}()
+})();
